a4b7fed43aa7ca5f588693fe3b2f09a7bfc32a06
Bence Balogh <benceeok@gmail.com>
2022-08-15 22:30:03 +0200

    Update __init__.py

--- src/polymesh/__init__.py
+++ src/polymesh/__init__.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
-__version__ = "0.0.3b"
+__version__ = "0.0.3"
 __description__ = "A Python package to build, manipulate and analyze polygonal meshes."
 
 from .space import PointCloud
 from .space import CartesianFrame
-#from .tri.triang import triangulate
-#from .grid import grid, Grid
-#from .tri.trimesh import TriMesh
-#from .tet.tetmesh import TetMesh
 from .polydata import PolyData
 from .linedata import LineData
 from .linedata import LineData as PolyData1d
